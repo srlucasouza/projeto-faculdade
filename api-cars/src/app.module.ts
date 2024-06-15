d9a2022d84dda4df26a1b347831c45816f59b4b6
Lucas Souza <130512380+srlucasouza@users.noreply.github.com>
2024-06-15 17:14:08 -0300

    Update app.module.ts

--- api-cars/src/app.module.ts
+++ api-cars/src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://srlucasbsouza:bo6U52K6LBqkmnc9@cloud-car.xxwpi9y.mongodb.net/?retryWrites=true&w=majority&appName=cloud-car'), CarsModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://srlucasbsouza:<password>@cloud-car.xxwpi9y.mongodb.net/?retryWrites=true&w=majority&appName=cloud-car'), CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
